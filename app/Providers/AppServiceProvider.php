0f2f57301ac3314f01bac8d39ec66c6be3de6801
Tidiane <gaye95ahmeth@gmail.com>
2020-01-07 00:19:25 +0000

    probleme de migration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Providers;
-
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use App\User;
 use Illuminate\Support\Facades\Gate;
@@ -36,6 +36,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema :: defaultStringLength (191); 
+        Schema::defaultStringLength(191); 
     }
 }
