b46f1ae76d00ae12103963c59b6e22d33fc8e3c9
CheikhTidianeGaye <gaye95ahmeth@gmail.com>
2020-02-13 16:53:50 +0000

    controle des dossiers

--- resources/views/medecin/folder.blade.php
+++ resources/views/medecin/folder.blade.php
@@ -339,6 +339,88 @@
                         </div>
                     </div>
             </div> 
-            
-                                                            
+
+             <div class="modal fade" id="myModalConsult">
+                <div class="modal-dialog modal-lg">
+                    <div class="modal-content">
+                        <!-- Modal Header -->
+                        <div class="modal-header">
+                        <a class="navbar-brand d-none d-sm-inline-block form-inline mr-auto ml-md-3 mb-md-3 my-2 my-md-0 mw-100" href="index.html"><img src="{{asset('img/core-img/logo.png')}}" alt="Logo"></a>
+                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        </div>                        
+                        <!-- Modal body -->
+                        <div class="modal-body container">
+                            <form action="" method="post">
+                            @csrf
+                           
+                                <div class="row">
+                                    <div class="form-group col-12 col-sm-12 col-md-6">
+                                        <label for="inputEmail" class="col-sm-2 ">Date prise<span style="background-colol:red;">*</span></span></label>
+                                        <div class="col-sm-10">
+                                            <input type="date" class="form-control" id="date" name="date_prise" >
+                                        </div>
+                                    </div>
+                                    <div class="form-group col-12 col-sm-12 col-md-6">
+                                        <label for="inputPassword" class="col-sm-2 ">Poids</label>
+                                        <div class="col-sm-10">
+                                            <input type="text" class="form-control" id="nom" name="nom" placeholder="Entrer Nom">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row ">
+                                    <div class="form-group col-12 col-sm-12 col-md-6">
+                                        <label for="inputPassword" class="col-sm-2 ">Tension</label>
+                                        <div class="col-sm-10">
+                                            <input type="date" class="form-control" id="date" name="date" placeholder="">
+                                        </div>
+                                    </div>
+                                    <div class="form-group col-12 col-sm-12 col-md-6">
+                                        <label for="inputPassword" class="col-sm-2 ">Temperature</label>
+                                        <div class="col-sm-10">
+                                            <input type="text" class="form-control" id="lieu" name="lieu" placeholder="Entrer Lieu">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                        <div class="form-group col-12 col-sm-12 col-md-6">
+                                            <label for="inputPassword" class="col-sm-2 ">Pression</label>
+                                            <div class="col-sm-10">
+                                                <input type="text" class="form-control" id="adresse" name="adresse" placeholder="Entrer Adresse">
+                                            </div>
+                                        </div>
+                                       
+                                    <div class="form-group col-12 col-sm-12 col-md-6">
+                                        <label for="inputPassword" class="col-sm-2 ">Telephone</label>
+                                        <div class="col-sm-10">
+                                            <input type="text" class="form-control" id="phone" name="phone" placeholder="Entrer Telephone"">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="form-group col-12 col-sm-12 col-md-6">
+                                        <label for="inputPassword" class="col-sm-2 ">Profession</label>
+                                        <div class="col-sm-10">
+                                            <input type="text" class="form-control" id="profession" name="profession" placeholder="Entrer Profession">
+                                        </div>
+                                    </div>
+                                    <div class="form-group col-12 col-sm-12 col-md-6">
+                                        <label for="sexe" class="col-sm-4 ">Sexe</label>
+                                        <div class="col-sm-10">
+                                            <select name="genre" id="genre" class="form-control">
+                                                <option value="masculin">Masculin</option>
+                                                <option value="feminin">Feminin</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="">         
+                                    <button type="submit" class="btn btn-success">Enregistrer</button>
+                                    <button type="reset" class="btn btn-danger" data-dismiss="modal">Close</button>
+                                </div>
+                            </form>
+                        </div>      
+                        <!-- Modal footer -->                
+                    </div>
+                </div>
+            </div>                                               
     @endsection
