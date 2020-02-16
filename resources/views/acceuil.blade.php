b85c9e5d7938f64aff5c7c36f35387c1b8d9993c
Tidiane <gaye95ahmeth@gmail.com>
2020-02-16 14:58:04 +0000

    image d'accueuil

--- resources/views/acceuil.blade.php
+++ resources/views/acceuil.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')  
         @section('content')
-            <div class="h-100" style="background-image: url(../images/anesthesiology.jpg);background-size:100%;">
+            <div class="h-100" style="background-image: url(../images/anesthesiology.jpg);background-size:100%;background-repeat:no-repeat;">
             
             </div>
     @endsection
